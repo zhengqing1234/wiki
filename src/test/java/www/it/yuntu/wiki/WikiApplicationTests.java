84a750bcbdfda87ce1b0cddfb0fdff76058c891f
admin <123123>
2021-07-08 17:59:39 +0800

    2-3 init

---
+++ src/test/java/www/it/yuntu/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package www.it.yuntu.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
