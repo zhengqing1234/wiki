91a4b71c188cb504368a4b84978b65e022dccadd
admin <123123>
2021-07-12 10:05:19 +0800

    2-8 配置文件

--- src/main/java/www/it/yuntu/wiki/controller/TestController.java
+++ src/main/java/www/it/yuntu/wiki/controller/TestController.java
@@ -1,16 +1,20 @@
 package www.it.yuntu.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
+    @Value("${config.txt}")
+    private String name;
 
     @GetMapping("hello")
     public String Hello(){
-        return "hello 美女";
+        return "hello 美女"+name;
     }
+
     @PostMapping("helloPost")
-    public String helloPOST(){
+    public String HelloPOST(){
         return "hello 帅哥";
     }
 
